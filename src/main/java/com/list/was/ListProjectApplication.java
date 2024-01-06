dfd01c111eb6f461b9040bc94b21357d7124d121
dongheehan <sms445566@naver.com>
2024-01-06 16:31:28 +0900

    test333

---
+++ src/main/java/com/list/was/ListProjectApplication.java
@@ -0,0 +1,13 @@
+package com.list.was;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/list/was/ListProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.list.was;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
